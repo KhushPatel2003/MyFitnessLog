330171cefc3eb6429d292ecd1069bfb33d5e9e30
KhushPatel2003 <64615690+KhushPatel2003@users.noreply.github.com>
2021-06-27 00:30:14 -0400

    Added LandingPage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../../styles/App.css';
+import HeroSection from '../LandingPage/LandingPage';
 
 export default function Home() {
     return (
-        <>  
+        <>
+            <HeroSection />  
         </>
     );
 }
