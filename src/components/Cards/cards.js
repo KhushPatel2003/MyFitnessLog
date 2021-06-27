38e6cc884d1818c52ecbc469e6141e205171769c
KhushPatel2003 <64615690+KhushPatel2003@users.noreply.github.com>
2021-06-27 01:30:29 -0400

    added cards

---
+++ src/components/Cards/cards.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import CardItem from '../CardItem/Carditems';
+import '../../styles/cards.css';
+import lose from '../../images/weight-loss-clipart-transparent-1.PNG';
+import gainPIC from '../../images/gain.PNG';
+import maintainePIC from '../../images/maintaine.PNG';
+import workout from '../../images/exercise.PNG';
+import tracking from '../../images/trackingCALS.PNG';
+
+function Cards() {
+    return (
+        <div className="cards">
+            <h1>All your fitness needs, fulfuilled.!</h1>
+            <br />
+            <br />
+            <div className="cards__container">
+                <div className="cards__wrapper">
+                    <ul className="cards__items">
+                        <CardItem 
+                        src={lose}
+                        text="Our program will calculate how many calories you should eat in a day to lose atleast 1 pound per week granted that you exercise and burn more calories as well. "
+                        // label="Monitor"
+                        // path="/services"
+                        />
+                        <CardItem 
+                        src={gainPIC}
+                        text="Our program will also calculate how many calories you should eat in a day to gain atleast 1 pound per week granted that you exercise and burn more calories as well. "
+                        // label="Detect"
+                        // path="/services"
+                        />
+                        <CardItem 
+                        src={maintainePIC}
+                        text="If you just want to maintain your weight then with our program we will provide the user with the amount of calories they should eat in a day based on their activity level to maintain their weight. "
+                        // label="Alert"
+                        // path="/services"
+                        />
+                    </ul>
+                    <ul className="cards__items">
+                    <CardItem 
+                        src={workout}
+                        text="We also offer workout tracking. You can input the amount of exercising you have done as well as teh amount of sets and reps perform with the weight. In the end a table will appear showing the user all the exercises they have done and the details of the exercise for their records. "
+                        // label="Alert"
+                        // path="/services"
+                        />
+                        <CardItem 
+                        src={tracking}
+                        text="You can also input what you ate for breakfast, lunch and dinner as well as snacks and drinks and their calories. Our program will then add up all those calories and tell you how many you have consumed as well as if you are on track to meet your goal or not."
+                        // label="Alert"
+                        // path="/services"
+                        />
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Cards
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import '../../styles/App.css';
 import HeroSection from '../LandingPage/LandingPage';
 import Navbar from '../navbar/navbar';
+import Cards from '../Cards/cards';
 
 export default function Home() {
     return (
         <>
           <Navbar/>
-          <HeroSection />  
+          <HeroSection />
+          <Cards />  
         </>
     );
 }
